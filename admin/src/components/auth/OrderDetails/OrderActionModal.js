293b54564d9f8f982c4e8e4d858490d1f055453f
unknown <sakarim9124@gmail.com>
2023-12-14 23:15:51 +0530

    updated

--- admin/src/components/auth/OrderDetails/OrderActionModal.js
+++ admin/src/components/auth/OrderDetails/OrderActionModal.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Modal, Radio, Select } from "antd";
+import React, { useState, useEffect } from "react";
+import { Modal, Radio, Select, Button, Form } from "antd";
 
 const { Option } = Select;
 
@@ -9,53 +9,65 @@ const OrderActionModal = ({
   setIsModalVisible,
   handleOrderAction,
 }) => {
+  const [form] = Form.useForm();
   const [action, setAction] = useState("accept");
-  const [declineReason, setDeclineReason] = useState("");
 
-  const handleOk = () => {
-    handleOrderAction({ action, declineReason });
-    // setIsModalVisible(false);
-  };
+  useEffect(() => {
+    // Reset form when the modal is closed or a new item is selected
+    form.resetFields();
+    setAction("accept");
+  }, [isModalVisible, selectedItem, form]);
 
-  const handleCancel = () => {
+  const onFormFinish = (values) => {
+    handleOrderAction(values);
     setIsModalVisible(false);
-  };
-
-  const handleActionChange = (value) => {
-    setAction(value);
-    if (value === "accept") {
-      setDeclineReason(""); // Reset decline reason if action is accept
-    }
-  };
-
-  const handleReasonChange = (e) => {
-    setDeclineReason(e.target.value);
   };
 
   return (
     <Modal
       title="Order Action"
       visible={isModalVisible}
-      onOk={handleOk}
-      onCancel={handleCancel}
+      onCancel={() => setIsModalVisible(false)}
+      footer={null}
       maskClosable={false}
     >
-      <Select
-        defaultValue="accept"
-        style={{ width: "100%" }}
-        onChange={handleActionChange}
+      <Form
+        form={form}
+        onFinish={onFormFinish}
+        initialValues={{ action: "accept", reason: "" }}
       >
-        <Option value="accept">Accept</Option>
-        <Option value="reject">Reject</Option>
-      </Select>
+        <Form.Item
+          name="action"
+          rules={[{ required: true, message: "Please select an action" }]}
+        >
+          <Select
+            style={{ width: "100%" }}
+            onChange={(value) => setAction(value)}
+          >
+            <Option value="accept">Accept</Option>
+            <Option value="reject">Reject</Option>
+          </Select>
+        </Form.Item>
 
-      {action === "reject" && (
-        <Radio.Group onChange={handleReasonChange} value={declineReason}>
-          <Radio value="restaurantClosed">Restaurant Closed</Radio>
-          <Radio value="outOfStock">Out of Stock</Radio>
-          <Radio value="other">Other</Radio>
-        </Radio.Group>
-      )}
+        {action === "reject" && (
+          <Form.Item
+            name="reason"
+            rules={[{ required: true, message: "Please select a reason" }]}
+          >
+            <Radio.Group>
+              <Radio value="restaurantClosed">Restaurant Closed</Radio>
+              <Radio value="outOfStock">Out of Stock</Radio>
+              <Radio value="other">Other</Radio>
+            </Radio.Group>
+          </Form.Item>
+        )}
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
